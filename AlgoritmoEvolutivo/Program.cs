d09a4b9db89902f06fed786659e95ac3990e92db
JUANDP28 <48769717+JUANDP28@users.noreply.github.com>
2020-04-18 23:18:51 -0500

    Proyecto Finalizado
    Se termino el algoritmo, que da pendiente algunas dudas, respecto al proyecto, pero ya funciona y es utilizable

--- AlgoritmoEvolutivo/Program.cs
+++ AlgoritmoEvolutivo/Program.cs
@@ -6,6 +6,7 @@ namespace AlgoritmoEvolutivo {
 
         List<Individuo> poblacion = new List<Individuo>();
         List<Individuo> padres = new List<Individuo>();
+        List<Individuo> hijos = new List<Individuo>();
         int numeroEvaluaciones = 0;
 
         /// <summary>
@@ -111,57 +112,226 @@ namespace AlgoritmoEvolutivo {
         }
 
         /// <summary>
-        /// Metodo que obtiene los 5 padres para la cruza
+        /// Metodo que obtiene los 5 padres aleatorios y
+        /// y selecciona los 2 mejores
         /// </summary>
         public void SeleccionarPadres () {
 
-            int minimoChoques = MinimoDeChoques();
-            int iterador = 0;
+            for (int i = 0; i < 5; i++) {
+                padres.Add(ObtenerIndividuo(NumeroAleatorioPoblacion()));
+            }
 
-            while (padres.Count < 5) {
+            /*foreach (Individuo elemento in padres) {
+                Console.WriteLine("Calidad: " + elemento.GetCalidad());
+            }*/
 
-                foreach (Individuo elemento in poblacion) {
+            while (padres.Count != 2) {
 
-                    if (elemento.GetCalidad() == (minimoChoques + iterador)) {
+                int iterador = 1;
+                int posición = 0;
+                int peorCalidad = 0;
 
-                        if (padres.Count != 5) {
-                            padres.Add(elemento);
+                foreach (Individuo elemento in padres) {
+
+                    if (iterador == 1) {
+
+                        posición = iterador;
+                        peorCalidad = elemento.GetCalidad();
+
+                    } else {
+
+                        if (elemento.GetCalidad() > peorCalidad) {
+
+                            posición = iterador;
+                            peorCalidad = elemento.GetCalidad();
                         }
                     }
+
+                    iterador++;
                 }
-                iterador++;
+
+                padres.Remove(ObtenerIndividuoPadres(posición));
             }
         }
 
         /// <summary>
-        /// Metodo que calcula el minimo de choques
+        /// Metodo que nos da un número aleatorio entre 1 y 101
         /// </summary>
-        /// <returns>varible de tipo int con el minimo de choques</returns>
-        public int MinimoDeChoques () {
+        /// <returns>número entre 1 y 101</returns>
+        public int NumeroAleatorioPoblacion () {
+            Random random = new Random();
+            return random.Next(1, 101);
+        }
 
-            int minimoChoques = 0;
+        /// <summary>
+        /// Metodo que devuelve un Individuo segun su posición
+        /// </summary>
+        /// <param name="posicion">variable de tipo int con la
+        /// posición del individuo deseado</param>
+        /// <returns>Individuo localizado en esa posición</returns>
+        public Individuo ObtenerIndividuo (int posicion) {
+
             int iterador = 1;
 
             foreach (Individuo elemento in poblacion) {
 
-                if (iterador == 1) {
+                if (iterador == posicion) {
 
-                    minimoChoques = elemento.GetCalidad();
+                    return elemento;
 
                 } else {
 
-                    if (minimoChoques > elemento.GetCalidad()) {
+                    iterador++;
+                }
 
-                        minimoChoques = elemento.GetCalidad();
-                    }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Metodo que Obtiene un Individuo de la lista padres
+        /// </summary>
+        /// <returns>varible de tipo Individuo que se solicita</returns>
+        public Individuo ObtenerIndividuoPadres (int posicion) {
+
+            int iterador = 1;
+
+            foreach (Individuo elemento in padres) {
+
+                if (iterador == posicion) {
+
+                    return elemento;
                 }
 
                 iterador++;
             }
-            return minimoChoques;
-        }
-        
 
+            return null;
+        }
+
+        /// <summary>
+        /// Metodo que cruza a los 2 mejores padres
+        /// </summary>
+        public void CruzaPadres () {
+
+            int lugarCorte = NumeroAleatorioCruza();
+            int [] solucion1 = new int [8];
+
+            for (int i = 0; i < lugarCorte; i++) {
+
+                solucion1 [i] = ObtenerIndividuoPadres(1).GetSolucion() [i];
+            }
+            for (int i = lugarCorte; i < ObtenerIndividuo(2).GetSolucion().Length; i++) {
+
+                solucion1 [i] = ObtenerIndividuoPadres(2).GetSolucion() [i];
+            }
+
+            if (ProbabilidadMutacion()) {
+
+                solucion1 [NumeroAleatorio()] = NumeroAleatorio();
+            }
+
+            int [] solucion2 = new int [8];
+
+            for (int i = 0; i < lugarCorte; i++) {
+
+                solucion2 [i] = ObtenerIndividuoPadres(2).GetSolucion() [i];
+            }
+            for (int i = lugarCorte; i < ObtenerIndividuo(1).GetSolucion().Length; i++) {
+
+                solucion2 [i] = ObtenerIndividuoPadres(1).GetSolucion() [i];
+            }
+
+            if (ProbabilidadMutacion()) {
+
+                solucion2 [NumeroAleatorio()] = NumeroAleatorio();
+            }
+
+            Individuo hijo1 = new Individuo(solucion1);
+            Evaluacion(hijo1);
+            Individuo hijo2 = new Individuo(solucion2);
+            Evaluacion(hijo2);
+            hijos.Add(hijo1);
+            hijos.Add(hijo2);
+            padres.Clear();
+        }
+
+        /// <summary>
+        /// Metodo para obtener un número aleatorio
+        /// </summary>
+        /// <returns>Unvalor entre 1 y 7</returns>
+        public int NumeroAleatorioCruza () {
+            Random random = new Random();
+            return random.Next(1,7);
+        }
+
+        /// <summary>
+        /// Metodo que determina la probabilidad de mutación
+        /// </summary>
+        /// <returns>valor verdadero o falso</returns>
+        public bool ProbabilidadMutacion () {
+            Random random = new Random();
+            if (random.Next(1, 11) <9) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public void Sustitucion () {
+            while (poblacion.Count != 98) {
+
+                int iterador = 1;
+                int posición = 0;
+                int peorCalidad = 0;
+
+                foreach (Individuo elemento in poblacion) {
+
+                    if (iterador == 1) {
+
+                        posición = iterador;
+                        peorCalidad = elemento.GetCalidad();
+
+                    } else {
+
+                        if (elemento.GetCalidad() > peorCalidad) {
+
+                            posición = iterador;
+                            peorCalidad = elemento.GetCalidad();
+                        }
+                    }
+
+                    iterador++;
+                }
+
+               poblacion.Remove(ObtenerIndividuo(posición));
+            }
+
+            foreach (Individuo elemento in hijos) {
+                poblacion.Add(elemento);
+            }
+
+            hijos.Clear();
+        }
+
+        public bool Solucion () {
+            foreach (Individuo elemento in poblacion) {
+
+                if (elemento.GetCalidad() == 0) {
+                    Console.WriteLine("");
+                    Console.WriteLine("=============== SOLUCIÓN ===============");
+                    Console.WriteLine("");
+                    for (int i = 0; i < elemento.GetSolucion().Length; i++) {
+                        Console.WriteLine("( " + (i+1) + " , " + (elemento.GetSolucion()[i]+1) + " )");
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("========================================");
+                    Console.WriteLine("");
+                    return true;
+                }
+            }
+            return false;
+        }
 
         static void Main (string [] args) {
             Console.WriteLine("");
@@ -172,7 +342,22 @@ namespace AlgoritmoEvolutivo {
             menu.CrearPoblación();
             menu.EvaluarPoblacion();
 
-            menu.SeleccionarPadres();
+            bool bandera = false;
+
+            while (bandera != true) {
+
+                //menu.EvaluarPoblacion();
+                menu.SeleccionarPadres();
+                menu.CruzaPadres();
+                menu.Sustitucion();
+
+                if (menu.numeroEvaluaciones > 10000 || menu.Solucion() == true) {
+
+                    bandera = true;
+                }
+            }
+
+            Console.WriteLine("NUMERO DE EVALUCIONES: " + menu.numeroEvaluaciones);
         }
     }
 }
